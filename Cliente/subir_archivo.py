e7182a00361cd23f678175a8df998c1caae6ac1a
Hernani Gomes <106991487+heralex98@users.noreply.github.com>
2023-01-02 18:40:32 -0400

    funcion subir_archivo

--- Cliente/subir_archivo.py
+++ Cliente/subir_archivo.py
@@ -1,5 +1,7 @@
 from ftplib import FTP
 
+###valores de la conexion###
+
 host = '192.168.0.101'
 port = 5000
 user = 'user'
@@ -10,12 +12,33 @@ ftp.login(user, password)
 print(ftp.getwelcome())
 print(ftp.cwd(user))
 
-if 'test.txt' in ftp.nlst():
-    ftp.delete('test.txt')
-    with open('./archivos_de_prueba/test.txt', 'rb') as text_file:
-        ftp.storlines('STOR test.txt', text_file)
-else:
-    with open('./archivos_de_prueba/test.txt', 'rb') as text_file:
-        ftp.storlines('STOR test.txt', text_file)
+###valores de la funcion###
 
+archivo = 'horario.xlsx'
+ubicacion_archivo = './archivos_de_prueba/horario.xlsx'
 
+###funcion###
+
+def subir_archivo(archivo_a_subir, ubicacion):
+    if archivo_a_subir.endswith('.txt'):
+        if archivo in ftp.nlst():
+            ftp.delete(archivo_a_subir)
+            with open(ubicacion, 'rb') as text_file:
+                ftp.storlines('STOR ' + archivo_a_subir, text_file)
+                print('se subio el archivo correctamente')
+        else:
+            with open(ubicacion, 'rb') as text_file:
+                ftp.storlines('STOR ' + archivo_a_subir, text_file)
+                print('se subio el archivo correctamente')
+    else:
+        if archivo in ftp.nlst():
+            ftp.delete(archivo_a_subir)
+            with open(ubicacion, 'rb') as image_file:
+                ftp.storbinary('STOR ' + archivo_a_subir, image_file)
+                print('se subio el archivo correctamente')
+        else:
+            with open(ubicacion, 'rb') as image_file:
+                ftp.storbinary('STOR ' + archivo_a_subir, image_file)
+                print('se subio el archivo correctamente')
+
+subir_archivo(archivo, ubicacion_archivo)
